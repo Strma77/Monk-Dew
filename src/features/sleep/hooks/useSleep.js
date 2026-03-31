10eb94c2d6f8ffc416254eb916d18460f4a04fd7
Strma77 <vito.strmecki@gmail.com>
2026-03-31 09:44:22 +0200

    Added useSleep hook logic

---
+++ src/features/sleep/hooks/useSleep.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { createSleepEntry } from "../utils/sleepModel"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useSleep = () => {
+    const [sleep_entries, setSleep] = useState([]);
+
+        const loadData = async () => {
+        const stored = await AsyncStorage.getItem('sleep_entries');
+
+        if (stored) {
+            setSleep(JSON.parse(stored));
+        } else {
+            setSleep([]);
+        }
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    const saveToStorage = async (sleep) => {
+        await AsyncStorage.setItem('sleep_entries', JSON.stringify(sleep));
+    };
+    const addSleep = (date, hours, minutes) => {
+        const newSleep = createSleepEntry(date, hours, minutes);
+        const updatedSleep = [...sleep_entries, newSleep];
+        setSleep(updatedSleep);
+        saveToStorage(updatedSleep);
+    };
+    const deleteSleep = (id) => {
+        const deletedSleep = sleep_entries.filter(t => t.id !== id);
+        setSleep(deletedSleep);
+        saveToStorage(deletedSleep);
+    };
+    const updateSleep = (updatedEntry) => {
+        const updatedSleep = sleep_entries.map(t => t.id === updatedEntry.id ? updatedEntry : t);
+        setSleep(updatedSleep);
+        saveToStorage(updatedSleep)
+    }
+
+    return { sleep_entries, saveToStorage, addSleep, deleteSleep, updateSleep };
+}
